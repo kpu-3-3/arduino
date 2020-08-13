4034c90de20a793ca6ff0bd0f957de700364c904
hyesuyam <50814310+hyesuyam@users.noreply.github.com>
2020-08-14 00:30:31 +0900

    Update arduino_pill.ino

--- arduino_pill.ino
+++ arduino_pill.ino
@@ -28,24 +28,22 @@ void setup() {
   neo.show();
 }
 
+String myString="";
 
+
+
+
+ 
 void setNeopixel(){     //네오픽셀 동작 클래스
     
-     char num=(char)bt.read();
-      Serial.print("Received data : ");
-      Serial.write(num);
-      Serial.println("");
-            
-      if(num=='1'){ //알람발생 
-        
         Serial.println("start neopixel");
           int i=0;
           
           while(i<5){//일정시간 경과 후 알람 종료 
-                  char num2=(char)bt.read(); //사용자가 알람 종료   
-                  if(num2=='2') {
+                  char num=(char)bt.read(); //사용자가 알람 종료   
+                  if(num=='2') {
                     Serial.print("Received data : ");
-                    Serial.println(num2);
+                    Serial.println(num);
                     Serial.println("stop neopixel");
                     break; 
                   }
@@ -53,13 +51,7 @@ void setNeopixel(){     //네오픽셀 동작 클래스
                   i++;
                      
                }    
-          colorWipe(neo.Color(0,0,0),0);  //네오픽셀 off         
-      }
-      
-      else{
-        colorWipe(neo.Color(0,0,0),0);
-        Serial.println("stop neopixel");
-      }
+          colorWipe(neo.Color(0,0,0),0);  //네오픽셀 off              
 }
 
 void rainbow(uint8_t wait) {
@@ -85,7 +77,7 @@ void rainbowCycle(uint8_t wait) {
   }
 }
 
-uint32_t Wheel(byte WheelPos) {
+uint32_t Wheel(byte WheelPos) { //led별 색상 제어
   if(WheelPos < 85) {
    return neo.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
   } else if(WheelPos < 170) {
@@ -105,165 +97,194 @@ void colorWipe(uint32_t c, uint8_t wait){    //led마다 개별 색상 설정
   }
 }
 
-int prev=0; //알람 시간 비교 변수
-int cur=0;
 
-int set_1=0;//메세지 전송여부 상태 변수, 전송 전 상태=0 전송 후=1, 재전송방지
-int set_2=0;
-int set_3=0;
-int set_4=0;
-
-int cnt=1;
-
-void time_pill(){ //안드로이드에 데이터 전송
-
-     cur++;
-
-      avr_RED1=(avr_RED1+read_RED1)/2;
-      avr_RED2=(avr_RED2+read_RED2)/2;
-      avr_RED3=(avr_RED3+read_RED3)/2;
-      avr_RED4=(avr_RED4+read_RED4)/2;    
-        
-    
-    if(cur-prev==10){ //안드로이드에 데이터 전송 //테스트용이라 10
+void check_pill(int check_num){//적외선 센서로 약물 여부 확인
+   
+   if(check_num==1){
+    while(1){
+      read_RED1=digitalRead(RED1);
+      Serial.print("적외선 센서 1 : ");
+      Serial.println(read_RED1); //0일 때 약물 미복용 //1일 때 약물 복용
       
-      if(avr_RED1>=0.9) { //복용상태전송
-        Serial.println("1번 복용"); 
-        if(set_1==0){
-           Serial.println("1)메세지 전송");
-           set_1=1; //메세지 전송여부확인
-           bt.println("10");//복용 상태 전송
-          delay(1000);
-           }
-        } 
-      else {
-        Serial.println("1번 미복용");
-       // bt.println("18");
-        delay(1000);
-       }//미복용상태전송
-         
-      if(avr_RED2>=0.9) {
-        Serial.println("2번 복용");
-        if(set_2==0){
-          Serial.println("2)메세지 전송");
-          set_2=1;
-          bt.println("20");
-          delay(1000);
-          }
-        }
-      else {
-        Serial.println("2번 미복용");
-       // bt.println("28");
-        delay(1000);
-       }
+      if(read_RED1==1){
+        Serial.println("1번 복용");
+        bt.println("10");//복용 상태 전송
 
-      if(avr_RED3>=0.9){
-        Serial.println("3번 복용");
-        if(set_3==0){
-          Serial.println("3)메세지 전송");
-          set_3=1;
-          bt.println("30");
-          delay(1000);
-          }
-        }
-      else  {
-        Serial.println("3번 미복용");
-       // bt.println("38");
-        delay(1000);
-        }
+        break;
+      }
+      else{
+        Serial.println("1번 미복용");
+        bt.println("18");//미복용 상태 전송
         
-      if(avr_RED4>=0.9) {
+        setNeopixel();//미복용 알람
+      }
+      delay(10000);//테스트용이라 10초     
+    }
+   }
+   
+   else if(check_num==2){
+    while(1){
+      read_RED2=digitalRead(RED2);
+      Serial.print("적외선 센서 2 : ");
+      Serial.println(read_RED2); //0일 때 약물 미복용 //1일 때 약물 복용
+      
+      if(read_RED2==1){
+        Serial.println("2번 복용");
+        bt.println("20");//복용 상태 전송
+
+        break;
+      }
+      else{
+        Serial.println("2번 미복용");
+        bt.println("28");//미복용 상태 전송
+        
+        setNeopixel();//미복용 알람
+      }
+      delay(10000);//테스트용이라 10초     
+    }
+   }
+
+    
+   else if(check_num==3){
+    while(1){
+      read_RED3=digitalRead(RED3);
+      Serial.print("적외선 센서 3 : ");
+      Serial.println(read_RED3); //0일 때 약물 미복용 //1일 때 약물 복용
+      
+      if(read_RED3==1){
+        Serial.println("3번 복용");
+        bt.println("30");//복용 상태 전송
+
+        break;
+      }
+      else{
+        Serial.println("3번 미복용");
+        bt.println("38");//미복용 상태 전송
+        
+        setNeopixel();//미복용 알람
+      }
+      delay(10000);//테스트용이라 10초     
+    }
+   }
+    
+   else if(check_num==4){
+    while(1){
+      read_RED4=digitalRead(RED4);
+      Serial.print("적외선 센서 4: ");
+      Serial.println(read_RED4); //0일 때 약물 미복용 //1일 때 약물 복용
+      
+      if(read_RED4==1){
         Serial.println("4번 복용");
-        if(set_4==0){
-          Serial.println("4)메세지 전송");
-          set_4=1;
-          bt.println("40");
-          delay(1000);
-          }
-       }
+        bt.println("40");//복용 상태 전송
+        //약통 채우기 알람 함수 호출
+        break;
+      }
       else{
         Serial.println("4번 미복용");
-       // bt.println("48");
-        delay(1000);
-       }
-
-      if((avr_RED1<0.9)||(avr_RED2<0.9)||(avr_RED3<0.9)||(avr_RED4<0.9)) { //미복용상태일때 네오픽셀 동작
-      
-          rainbow(10);
-          colorWipe(neo.Color(0,0,0),0);
-        }
+        bt.println("48");//미복용 상태 전송
         
+        setNeopixel();//미복용 알람
+      }
+      delay(10000);//테스트용이라 10초     
+    }
    }
       
-    
-
-  if(cur-prev==20){ 
-     
-     if((avr_RED1>=0.9)&&(avr_RED2>=0.9)&&(avr_RED3>=0.9)&&(avr_RED4>=0.9)){ //약통이 빈 경우
-     
-      prev=20*cnt;
-      cnt++;
-
-      rainbow(10);
-      colorWipe(neo.Color(0,0,0),0);
-      
-      Serial.println("약통을 채우세요!\n");
-      //bt.println("약통을 채우세요!");
-      
-     }
-     
-    delay(3000);
-    
-    if((avr_RED1<0.9)&&(avr_RED2<0.9)&&(avr_RED3<0.9)&&(avr_RED4<0.9)) { //약통을 채운경우
-      
-      Serial.println("약통을 채웠습니다!\n");
-      
-      cur=0;
-      prev=0;
-
-      cnt=1;
-       
-      set_1=0;
-      set_2=0;
-      set_3=0;
-      set_4=0;
-       
+    else{
+      Serial.println("타입 오류2");
     }
-  }    
-   
+  }
   
-}
-
-void check_pill(){ //약물 유무 확인 클래스
-    
-    read_RED1=digitalRead(RED1);
-    read_RED2=digitalRead(RED2);
-    read_RED3=digitalRead(RED3);
-    read_RED4=digitalRead(RED4);
-
-    Serial.print("적외선 센서 1 : ");
-    Serial.println(read_RED1);
-    Serial.print("적외선 센서 2 : ");
-    Serial.println(read_RED2);
-    Serial.print("적외선 센서 3 : ");
-    Serial.println(read_RED3);
-    Serial.print("적외선 센서 4 : ");
-    Serial.println(read_RED4);
-    Serial.println("");
-    
-    time_pill();
+void fill_pill_box(){//약통 채웠는지 확인 
+  while(1){
+  for(int i=0;i<20;i++){
+    avr_RED1=(avr_RED1+read_RED1)/2;
+    avr_RED2=(avr_RED2+read_RED2)/2;
+    avr_RED3=(avr_RED3+read_RED3)/2;
+    avr_RED4=(avr_RED4+read_RED4)/2;
+  
+  if(avr_RED1>=0.9&&avr_RED2>=0.9&&avr_RED3>=0.9&&avr_RED4>=0.9){//약통이 빈 경우
+   setNeopixel();
+   Serial.println("약통을 채우세요");
+   bt.println("약통을 채우세요");
+   }
+  }
+  if((avr_RED1<0.9)&&(avr_RED2<0.9)&&(avr_RED3<0.9)&&(avr_RED4<0.9)) { //약통을 채운경우
+    Serial.println("약통을 채웠습니다!");
+    bt.println("약통을 채웠습니다");
+    break;
+  }
+  delay(1000);
+ }
+}  
+void time_pill(int type_num){
+     
+  switch(type_num){
+    case 1:
+      check_pill(1);
+      break;
       
-    delay(1000);
+    case 2:
+      check_pill(2);
+      break;
+    
+    case 3:
+      check_pill(3);
+      break;
+    
+    case 4:
+      check_pill(4);
+      delay(30000);    
+      fill_pill_box();//약통채우기 알람 발생
+      break;
+      
+    default : 
+      Serial.println("타입 오류1");
+      break;
+    }
+ }
+
+
+void receive_message(){
+    while(bt.available()){
+      char data=(char)bt.read();//메세지 저장
+      myString+=data;
+      delay(5);
+    }
+      
+    if(!myString.equals(""))
+    {
+      Serial.println(" send message : " + myString);
+      if(myString.equals("type1")){
+      setNeopixel();
+      delay(10000); //테스트용이라 10초 //나중에 1800*1000=1800000=30분으로 수정하기 
+      time_pill(1); 
+    }       
+    else if(myString.equals("type2")){
+      setNeopixel();
+      delay(10000); //테스트용이라 10초 //나중에 1800*1000=1800000=30분으로 수정하기 
+      time_pill(2);
+
+    }
+    else if(myString.equals("type3")){
+      setNeopixel();
+      delay(10000); //테스트용이라 10초 //나중에 1800*1000=1800000=30분으로 수정하기 
+      time_pill(3);
+    }
+    else if(myString.equals("type4")){
+      setNeopixel();
+      delay(10000); //테스트용이라 10초 //나중에 1800*1000=1800000=30분으로 수정하기 
+      time_pill(4);
+    }
+
+      myString="";
+    }        
+
+    
+
 }
 
 void loop() { 
-  
-    check_pill(); //적외선센서 동작   
-    if(bt.available())
-    {  
-      setNeopixel();
-      delay(2000);
-    }   
-    
-    
+
+  receive_message();
+
 }
